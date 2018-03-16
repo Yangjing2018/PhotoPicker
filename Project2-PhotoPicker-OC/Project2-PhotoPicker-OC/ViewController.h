cc40033e2e01deb76e43d6313f96e8eaa2695465
yangjing901 <yangjing901@qq.com>
2018-03-16 17:46:10 +0800

    demo - oc

---
+++ Project2-PhotoPicker-OC/Project2-PhotoPicker-OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project2-PhotoPicker-OC
+//
+//  Created by YangJing on 2018/3/16.
+//  Copyright © 2018年 YangJing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Project2-PhotoPicker-OC/Project2-PhotoPicker-OC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Project2-PhotoPicker-OC
+//
+//  Created by YangJing on 2018/3/16.
+//  Copyright © 2018年 YangJing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
