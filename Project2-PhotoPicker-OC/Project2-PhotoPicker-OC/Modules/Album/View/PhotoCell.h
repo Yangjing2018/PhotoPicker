f347c79926a947834d036fa43a328195ba8f7471
Yangjing <yangjing@wordoor.com>
2018-05-04 17:51:33 +0800

    no message

---
+++ Project2-PhotoPicker-OC/Project2-PhotoPicker-OC/Modules/Album/View/PhotoCell.h
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.h
+//  Project2-PhotoPicker-OC
+//
+//  Created by YangJing on 2018/5/4.
+//  Copyright © 2018年 YangJing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCell : UICollectionViewCell
+
+@end
---
+++ Project2-PhotoPicker-OC/Project2-PhotoPicker-OC/Modules/Album/View/PhotoCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.m
+//  Project2-PhotoPicker-OC
+//
+//  Created by YangJing on 2018/5/4.
+//  Copyright © 2018年 YangJing. All rights reserved.
+//
+
+#import "PhotoCell.h"
+
+@implementation PhotoCell
+
+@end
